b7d3fac56a15f7485967b304f5270720aca76d8a
Justin LeBlanc <jleblanc@polygon.technology>
2024-02-22 22:31:37 +0000

    Build a framework for Rapidsnark

--- build.sh
+++ build.sh
@@ -10,6 +10,10 @@ BJJ_LIBS="$XCF_DIR/babyjubjub/libs"
 
 CPOLY_DIR="$XCF_DIR/../c-polygonid"
 
+RS_DIR="$XCF_DIR/../rapidsnark"
+RS_MACOS_DIR="$RS_DIR/package_macos_arm64"
+RS_LIBS="$XCF_DIR/rapidsnark/libs"
+
 set -e
 
 mergeLibraries() {
@@ -29,7 +33,9 @@ mergeLibraries() {
 # echo "Cleaning..."
 
 mkdir -p libs
-rm -f $XCF_LIBS/libpolygonid-*.a
+rm -f $BJJ_LIBS/*.a
+rm -f $RS_LIBS/*.a
+rm -f $XCF_LIBS/*.a
 rm -rf *.xcframework
 rm -f *.zip
 
@@ -42,11 +48,55 @@ git pull
 make ios
 make bindings
 
+mkdir -p $XCF_INCLUDE
 cp target/bindings.h $XCF_INCLUDE/babyjubjub.h
 
+mkdir -p $BJJ_LIBS
 cp libs/libbabyjubjub-ios.a $BJJ_LIBS/libbabyjubjub-ios.a
 cp libs/libbabyjubjub-ios-sim.a $BJJ_LIBS/libbabyjubjub-ios-sim.a
 cp libs/libbabyjubjub-macos.a $BJJ_LIBS/libbabyjubjub-macos.a
+
+cd $XCF_DIR
+xcodebuild -verbose -create-xcframework \
+     -output BabyJubjub.xcframework \
+    -library $BJJ_LIBS/libbabyjubjub-macos.a \
+    -library $BJJ_LIBS/libbabyjubjub-ios-sim.a \
+    -library $BJJ_LIBS/libbabyjubjub-ios.a
+
+
+
+echo "Building rapidsnark..."
+
+RS_FILES=(librapidsnark libfr libfq libgmp)
+
+cd $RS_DIR
+
+git pull
+
+git submodule init
+git submodule update
+
+./build_gmp.sh macos_arm64
+mkdir -p build_prover_macos_arm64 && cd build_prover_macos_arm64
+cmake .. -DTARGET_PLATFORM=macos_arm64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$RS_MACOS_DIR
+make -j4 && make install
+
+mkdir -p $RS_LIBS
+
+for file in "${RS_FILES[@]}"; do
+  cp "$RS_MACOS_DIR/lib/$file.a" "$RS_LIBS/$file-macos.a"
+done
+
+rm -f $RS_LIBS/rapidsnark-*
+libtool -static -no_warning_for_no_symbols \
+  -o $RS_LIBS/rapidsnark-macos.a \
+  $RS_LIBS/*-macos.a
+
+
+cd $XCF_DIR
+xcodebuild -verbose -create-xcframework \
+    -output Rapidsnark.xcframework \
+    -library $RS_LIBS/rapidsnark-macos.a
 
 echo "Building c-polygon..."
 
@@ -56,10 +106,11 @@ git pull
 
 make ios-simulator
 make ios
-# TODO: Should we support darwin-amd64?
 make darwin-arm64
 
 cp ios/libpolygonid.h "$XCF_INCLUDE"
+
+mkdir -p $XCF_LIBS
 cp ios/libpolygonid-darwin-arm64.a $XCF_LIBS/cpolygonid-macos.a
 cp ios/libpolygonid-ios.a $XCF_LIBS/cpolygonid-ios.a
 cp ios/libpolygonid-ios-simulator.a $XCF_LIBS/cpolygonid-ios-sim.a
@@ -82,11 +133,7 @@ libtool -static -no_warning_for_no_symbols \
   
 echo "Building xcframeworks..."
 
-xcodebuild -verbose -create-xcframework \
-     -output BabyJubjub.xcframework \
-    -library $BJJ_LIBS/libbabyjubjub-macos.a \
-    -library $BJJ_LIBS/libbabyjubjub-ios-sim.a \
-    -library $BJJ_LIBS/libbabyjubjub-ios.a
+
 
 xcodebuild -verbose -create-xcframework \
      -output LibPolygonID.xcframework \
