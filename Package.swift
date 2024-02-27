6c49c9e4acb0c6a4907fcb82e6485db1e17c750a
Justin LeBlanc <jleblanc@polygon.technology>
2024-02-27 12:46:40 +0000

    Make zip files for each library

--- Package.swift
+++ Package.swift
@@ -14,13 +14,17 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "BabyJubjub",
-                      path: "BabyJubjub.xcframework"),
         .binaryTarget(name: "LibPolygonID",
-                      path: "LibPolygonID.xcframework"),
+                      url: "https://github.com/jrl351/LibPolygonID/releases/download/v0.0.5/libpolygonid.zip",
+                      checksum: "83d826d7d22bd6de032ff4193975a04d17d3ab2bbdb6e4fe7e5feb5996a62c8a"),
+        .binaryTarget(name: "BabyJubjub",
+                      url: "https://github.com/jrl351/LibPolygonID/releases/download/v0.0.5-bjj/babyjubjub.zip",
+                      checksum: "2e9df23e9f10b116974cbfd02fa923ac231b08ff4573fe28b11e13f620a2b1c6"),
         .binaryTarget(name: "WitnessCalc",
-                      path: "WitnessCalc.xcframework"),
+                      url: "https://github.com/jrl351/LibPolygonID/releases/download/v0.0.5-witness/witnesscalc.zip",
+                      checksum: "938efe7217930008d39b5ef88c08845f4f002a4443526b8c56297f237aeae588"),
         .binaryTarget(name: "Rapidsnark",
-                      path: "Rapidsnark.xcframework"),
+                      url: "https://github.com/jrl351/LibPolygonID/releases/download/v0.0.5-rs/witnesscalc.zip",
+                      checksum: "e4004df31851a761a7d716da91259739c114f626e0444b0c0ab04aa430052797"),
     ]
 )
