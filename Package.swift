d71b919a714b381b9f424d6a2dc914a5409a8c6f
Justin LeBlanc <jleblanc@polygon.technology>
2024-04-29 11:03:23 +0100

    Pull in CPolygonID xcframework from the repo directly

--- Package.swift
+++ Package.swift
@@ -26,8 +26,6 @@ let package = Package(
         .binaryTarget(name: "WitnessCalc",
                       url: "https://github.com/jrl351/LibPolygonID/releases/download/v0.0.6/witnesscalc.zip",
                       checksum: "3ba680a6aa146ebf679a8a923cbb3a220db1eca5c2930b52c0d41ef0f658f509"),
-        .binaryTarget(name: "CPolygonID",
-                      url: "https://github.com/jrl351/LibPolygonID/releases/download/v0.0.7/cpolygonid.zip",
-                      checksum: "22d1bce838d45578eee1ba4457efa8e77f98a36534addfbae45be496deb38e9a"),
+        .binaryTarget(name: "CPolygonID", path: "CPolygonID.xcframework"),
     ]
 )
